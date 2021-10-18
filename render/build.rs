a08910c232af526de0fe563cf2a5287bce99d147
AcrylicShrimp <led789zxpp@naver.com>
2021-10-18 22:58:34 +0900

    fix: correct the build script.

--- render/build.rs
+++ render/build.rs
@@ -1,6 +1,17 @@
-fn main() {
-    #[cfg(any(target_os = "linux", target_os = "macos", target_os = "windows"))]
-    {
-        println!("cargo:rustc-cfg=gl33");
+use std::env::{var as env_var, VarError};
+
+fn main() -> Result<(), VarError> {
+    match (
+        env_var("CARGO_CFG_TARGET_OS")?.as_str(),
+        env_var("CARGO_CFG_TARGET_ARCH")?.as_str(),
+    ) {
+        ("linux", "x86_64") | ("macos", "x86_64") | ("windows", "x86_64") => {
+            println!("cargo:rustc-cfg=gl33");
+        }
+        _ => {
+            println!("cargo:warning={}", "the given target is not available");
+        }
     }
+
+    Ok(())
 }

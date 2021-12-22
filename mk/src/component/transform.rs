918fcd4230cf509c0101c19f69dbc121b68557ef
AcrylicShrimp <led789zxpp@naver.com>
2021-12-22 16:03:49 +0900

    feat: split world and local properties.

--- mk/src/component/transform.rs
+++ mk/src/component/transform.rs
@@ -15,6 +15,12 @@ pub struct Transform {
     scale: PhantomData<Vec2>,
     #[lua_userfunc(get=lua_get_angle, set=lua_set_angle)]
     angle: PhantomData<f32>,
+    #[lua_userfunc(get=lua_get_local_position, set=lua_set_local_position)]
+    local_position: PhantomData<Vec2>,
+    #[lua_userfunc(get=lua_get_local_scale, set=lua_set_local_scale)]
+    local_scale: PhantomData<Vec2>,
+    #[lua_userfunc(get=lua_get_local_angle, set=lua_set_local_angle)]
+    local_angle: PhantomData<Vec2>,
 }
 
 impl Transform {
@@ -24,6 +30,9 @@ impl Transform {
             position: PhantomData,
             scale: PhantomData,
             angle: PhantomData,
+            local_position: PhantomData,
+            local_scale: PhantomData,
+            local_angle: PhantomData,
         }
     }
 
@@ -45,32 +54,74 @@ impl Transform {
     }
 
     fn lua_get_position<'lua>(&self, lua: &'lua Lua) -> LuaResult<LuaValue<'lua>> {
+        crate::transform::Transform::world_position(self.index, &use_context().transform_mgr())
+            .to_lua(lua)
+    }
+
+    fn lua_set_position(&self, value: LuaValue, lua: &Lua) -> LuaResult<()> {
+        crate::transform::Transform::set_world_position(
+            self.index,
+            &mut use_context().transform_mgr_mut(),
+            Vec2::from_lua(value, lua)?,
+        );
+        Ok(())
+    }
+
+    fn lua_get_scale<'lua>(&self, lua: &'lua Lua) -> LuaResult<LuaValue<'lua>> {
+        crate::transform::Transform::world_scale(self.index, &use_context().transform_mgr())
+            .to_lua(lua)
+    }
+
+    fn lua_set_scale(&self, value: LuaValue, lua: &Lua) -> LuaResult<()> {
+        crate::transform::Transform::set_world_scale(
+            self.index,
+            &mut use_context().transform_mgr_mut(),
+            Vec2::from_lua(value, lua)?,
+        );
+        Ok(())
+    }
+
+    fn lua_get_angle<'lua>(&self, lua: &'lua Lua) -> LuaResult<LuaValue<'lua>> {
+        crate::transform::Transform::world_angle(self.index, &use_context().transform_mgr())
+            .to_lua(lua)
+    }
+
+    fn lua_set_angle(&mut self, value: LuaValue, lua: &Lua) -> LuaResult<()> {
+        crate::transform::Transform::set_world_angle(
+            self.index,
+            &mut use_context().transform_mgr_mut(),
+            f32::from_lua(value, lua)?,
+        );
+        Ok(())
+    }
+
+    fn lua_get_local_position<'lua>(&self, lua: &'lua Lua) -> LuaResult<LuaValue<'lua>> {
         self.with_transform(|t| t.position).to_lua(lua)
     }
 
-    fn lua_set_position(&mut self, value: LuaValue, lua: &Lua) -> LuaResult<()> {
+    fn lua_set_local_position(&mut self, value: LuaValue, lua: &Lua) -> LuaResult<()> {
         self.with_transform_mut(move |t| {
             t.mark_as_dirty();
             Ok(t.position = Vec2::from_lua(value, lua)?)
         })
     }
 
-    fn lua_get_scale<'lua>(&self, lua: &'lua Lua) -> LuaResult<LuaValue<'lua>> {
+    fn lua_get_local_scale<'lua>(&self, lua: &'lua Lua) -> LuaResult<LuaValue<'lua>> {
         self.with_transform(|t| t.scale).to_lua(lua)
     }
 
-    fn lua_set_scale(&mut self, value: LuaValue, lua: &Lua) -> LuaResult<()> {
+    fn lua_set_local_scale(&mut self, value: LuaValue, lua: &Lua) -> LuaResult<()> {
         self.with_transform_mut(move |t| {
             t.mark_as_dirty();
             Ok(t.scale = Vec2::from_lua(value, lua)?)
         })
     }
 
-    fn lua_get_angle<'lua>(&self, lua: &'lua Lua) -> LuaResult<LuaValue<'lua>> {
+    fn lua_get_local_angle<'lua>(&self, lua: &'lua Lua) -> LuaResult<LuaValue<'lua>> {
         self.with_transform(|t| t.angle).to_lua(lua)
     }
 
-    fn lua_set_angle(&mut self, value: LuaValue, lua: &Lua) -> LuaResult<()> {
+    fn lua_set_local_angle(&mut self, value: LuaValue, lua: &Lua) -> LuaResult<()> {
         self.with_transform_mut(move |t| {
             t.mark_as_dirty();
             Ok(t.angle = f32::from_lua(value, lua)?)

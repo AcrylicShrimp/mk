649647a57e5c9a8c3bd383bf2e0741383c9ca824
AcrylicShrimp <led789zxpp@naver.com>
2022-05-22 22:05:15 +0900

    feat: add support for the aarch64 macos.

--- render/build.rs
+++ render/build.rs
@@ -5,7 +5,7 @@ fn main() -> Result<(), VarError> {
         env_var("CARGO_CFG_TARGET_OS")?.as_str(),
         env_var("CARGO_CFG_TARGET_ARCH")?.as_str(),
     ) {
-        ("linux", "x86_64") | ("macos", "x86_64") | ("windows", "x86_64") => {
+        ("linux", "x86_64") | ("macos", "x86_64") | ("macos", "aarch64") | ("windows", "x86_64") => {
             println!("cargo:rustc-cfg=gl33");
         }
         _ => {
